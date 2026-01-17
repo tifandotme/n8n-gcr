fc93064b2b3b5ad7df855785efc432ed21e6d443
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2026-01-17 16:35:35 +0700

    refactor(terraform): rename ci_sa_key output to base64_encoded_service_account_json for clarity

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "ci_sa_key" {
+output "base64_encoded_service_account_json" {
   description = "CI Service Account Key"
   value       = google_service_account_key.ci_sa_key.private_key
   sensitive   = true
